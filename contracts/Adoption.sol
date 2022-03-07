b2ffd7fff2b230a7bebd0ac98605854163c3668f
benoit <benoit@assetik.com>
2022-03-07 16:26:22 +0400

    update

---
+++ contracts/Adoption.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.8.15;
+
+contract Adoption {
+    address[16] public adopters;
+    
+    function adopt(uint petId) public returns(uint) {
+        require(petId >= 0 && petId <= 15, "Oops! we don't have a pet with the given tag.");
+        require(address(adopters[petId]) == 0x0000000000000000000000000000000000000000, "Oh snap! it seems someone already adopted that cutie.");
+        
+        adopters[petId] = msg.sender;
+        return petId;        
+    }
+    
+    function getAdopters() public view returns(address[16] memory) {
+        return adopters;
+    }
+}
